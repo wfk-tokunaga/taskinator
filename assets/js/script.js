6f5ab6d8b3469b7c993ac22ef3a9514861f28e8a
Will Tokunaga <wktokunaga@gmail.com>
2022-03-24 12:08:11 -0700

    prevented default for form submit event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,55 @@
-var buttonEl = document.querySelector('#save-task');
 var taskToDoEl = document.querySelector('#tasks-to-do');
-console.log(buttonEl);
+var formEl = document.querySelector('#task-form');
 
 //Make sure we're only targeting the specific button
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     taskToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
 
-
-// getName takes a function as a parameter
-var getName = function (callBack) {
-    var name = prompt("Enter name:");
-    callBack(name);
-}
-
-var doSomethingWithName = function (name, somethingFunction) {
-    somethingFunction(name);
-}
-
-function printName(name) {
-    console.log(name);
-}
-
-function printDoubleName(name) {
-    console.log(`${name}${name}`);
-}
-
-// var counter = 5;
-// var everySec = function () {
-//     console.log(counter);
-//     counter--;
-//     if (counter === 0) {
-//         console.log('sad');
-//         clearInterval(startCountdown);
-//     };
+// // getName takes a function as a parameter
+// var getName = function (callBack) {
+//     var name = prompt("Enter name:");
+//     callBack(name);
 // }
 
-// var startCountdown = setInterval(everySec, 500);
+// var doSomethingWithName = function (name, somethingFunction) {
+//     somethingFunction(name);
+// }
 
-var sayHello = function () {
-    console.log("Hello");
-};
+// function printName(name) {
+//     console.log(name);
+// }
 
-var timedGreeting = setTimeout(sayHello, 2000);
-clearTimeout(timedGreeting);
+// function printDoubleName(name) {
+//     console.log(`${name}${name}`);
+// }
 
-// startCountdown;
-//We call a function like getName, which takes a function as a parameter
-//It uses the callback function parameter in it's execution
+// // var counter = 5;
+// // var everySec = function () {
+// //     console.log(counter);
+// //     counter--;
+// //     if (counter === 0) {
+// //         console.log('sad');
+// //         clearInterval(startCountdown);
+// //     };
+// // }
+
+// // var startCountdown = setInterval(everySec, 500);
+
+// var sayHello = function () {
+//     console.log("Hello");
+// };
+
+// var timedGreeting = setTimeout(sayHello, 2000);
+// clearTimeout(timedGreeting);
+
+// // startCountdown;
+// //We call a function like getName, which takes a function as a parameter
+// //It uses the callback function parameter in it's execution
