0f654e649ffa135367a71bfcec360cb622504ba5
Will Tokunaga <wktokunaga@gmail.com>
2022-03-24 12:55:03 -0700

    added data validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var taskFormHandler = function (event) {
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //Validating data
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput,
